0ce430b5b2f69eeb32295db8610bace98fbc2e4c
David Wenk <davidwenk@icloud.com>
2024-05-21 11:16:23 +0200

    add new list item and show all lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,38 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all 
+    @list = List.new
+
+
+    if @lists.empty?
+      @status = "No lists yet. Please add a new list"
+    else
+      @status = "You have #{@lists.length} movie lists"
+    end 
   end
 
   def show
+    @list = List.find(params[:id])
+    # @movies = @List.movies
   end
 
   def new
+    # @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to lists_path, notice: "list was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private 
+
+  def list_params 
+    params.require(:list).permit(:name)
+  end 
 end
